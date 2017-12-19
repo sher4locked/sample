848332f518cf4548bd252e5d02f7a95bc8511bbe
Anush S <anush.sekharamantri@gmail.com>
2017-12-19 16:06:14 +0530

    Mod run

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export $JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.ym
  echo $JAVA_OPTS
  echo $TOMCAT_HOME
 
-$TOMCAT_HOME/bin/catalina.sh stop
-sleep 100
-$TOMCAT_HOME/bin/catalina.sh run
+#$TOMCAT_HOME/bin/catalina.sh stop
+#sleep 100
+#$TOMCAT_HOME/bin/catalina.sh run
  
