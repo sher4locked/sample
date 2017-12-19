c2b423be9a4ad2e43105e0cba5308d8722ab9954
Anush S <anush.sekharamantri@gmail.com>
2017-12-19 14:58:05 +0530

    Adding run

---
+++ run.sh
@@ -0,0 +1,8 @@
+echo $JAVA_OPTS
+export $JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml
+ echo $JAVA_OPTS
+
+$TOMCAT_HOME/bin/catalina.sh stop
+sleep 100
+$TOMCAT_HOME/bin/catalina.sh run
+ 
