cc8bb5d28c134a037ac4806a923f188790bf6454
Anush S <anush.sekharamantri@gmail.com>
2017-12-19 15:02:11 +0530

    Offloading to .sh file

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo $JAVA_OPTS
 export $JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml
  echo $JAVA_OPTS
+ echo $TOMCAT_HOME
 
 $TOMCAT_HOME/bin/catalina.sh stop
 sleep 100
