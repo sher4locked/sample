51db7900c68fa44ecb5c2121534b0e0ef1a1e589
Anush S <anush.sekharamantri@gmail.com>
2017-12-19 16:33:02 +0530

    Moving wget to sh file, adding OPTS to catalina

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM tomcat:7
 
-RUN wget -O /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
-RUN wget -O /opt/tomcat.yml https://raw.githubusercontent.com/OpsMx/restapp/master/prometheus/tomcat.yml
-
 COPY run.sh /usr/local/bin/run.sh
 RUN chmod +x /usr/local/bin/run.sh
 
--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 echo $JAVA_OPTS
-export $JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml
- echo $JAVA_OPTS
- echo $TOMCAT_HOME
+#export $JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml
 
-echo '$JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml' >> $TOMCAT_HOME/bin/catalina.sh
+wget -O /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
+
+wget -O /opt/tomcat.yml https://raw.githubusercontent.com/OpsMx/restapp/master/prometheus/tomcat.yml
+
+export JAVA_OPTS="$JAVA_OPTS -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml"
+
+echo $JAVA_OPTS
+echo $TOMCAT_HOME
+
+echo '$JAVA_OPTS=-javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml' >> /usr/local/tomcat/bin/catalina.sh
 
 #$TOMCAT_HOME/bin/catalina.sh stop
 #sleep 100
